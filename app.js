4d996970e0a0e0c81af131fdf3e2172504f33ce5
Vasquez <Cesar.Vasquez@xerox.com>
2020-03-03 15:03:17 -0600

    Bar Chart

--- app.js
+++ app.js
@@ -1,147 +1,46 @@
-var quotes = [
-  {
-    quote: "I see dead people.",
-    movie: "The Sixth Sense",
-    year: 1800,
-    rating: "PG-13"
-  }, {
-    quote: "May the force be with you.",
-    movie: "Star Wars: Episode IV - A New Hope",
-    year: 1850,
-    rating: "PG"
-  }, {
-    quote: "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?",
-    movie: "Dirty Harry",
-    year: 1971,
-    rating: "R"
-  }, {
-    quote: "You had me at 'hello.'",
-    movie: "Jerry Maguire",
-    year: 1200,
-    rating: "R"
-  }, {
-    quote: "Just keep swimming. Just keep swimming. Swimming, swimming, swiming.",
-    movie: "Finding Nemo",
-    year: 2800,
-    rating: "G"
-  },
-  {
-    quote: "It's necessary",
-    movie: "Interstellar",
-    year: 1700,
-    rating: "PG"
-  },
-  {
-    quote: "It's necessary",
-    movie: "Interstellar",
-    year: 1400,
-    rating: "PG"
-  }
-];
-
-var width = 500
+var width = 800
 var height = 300
-var barPadding = 10
-var numBars = quotes.length
-var barWidth = width / numBars - barPadding
+var paddingBar = 10
 
-var maxYear = d3.max(quotes, d => d.year)
-var minYear = d3.min(quotes, d => d.year)
+var firstAttemptDates = dates.map(dates => dates.FirstAttempt)
+var uniquefirstAttemptDates = [...new Set(firstAttemptDates)]
+
+var widthBar = width / uniquefirstAttemptDates.length - paddingBar - 10
+
+var dateMax = d3.max(dates, d => d.FirstAttempt)
+var dateMin = d3.min(dates, d => d.FirstAttempt)
+
 
 var yScale = d3.scaleLinear()
-                .domain([0, maxYear])//data
-                .range([height, 0])//box size
+                .domain([dateMin, dateMax])
+                .range([height, 0])
 
-var svg = d3.select("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .style("padding-left","50px")
-
-var yAxis = d3.axisRight(yScale)
-
-svg
-  .append("g")
-  .style("padding-left","-10px")
-  .call(yAxis)
-
-svg
-  .selectAll("rect")
-  .data(quotes)
-    .enter()
-    .append("rect")
-      .attr("x", (d,i) => (barPadding + barWidth) * i)
-      .attr("y", (d) => yScale(d.year))
-      .attr("width", barWidth)
-      .attr("height", d => height - yScale(d.year))
-      .attr("fill", "green")
-  
-
-  
+var  yScaleAxis = d3.scaleLinear()
+                .domain([0, dates.length])
+                .range([height, 0])
 
 
-/*var ratesData = quotes.map(rates => 
-  "<option value='"+ rates.rating +"'>"+ rates.rating + "</options>"
-  )
+var svgDates = d3.select("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("border", "1px solid black")
 
-var rates = [...new Set(ratesData)]
+  console.log(uniquefirstAttemptDates)
 
-rates.push('<option value="all">All</option>')
+var yAxis = d3.axisRight(yScaleAxis)
 
-document.getElementById("rates").innerHTML = rates
+svgDates.append("g")
+        .style("transform","translate(1px,10px)")
+        .call(yAxis)
 
-var colors = {
-  "G": "green",
-  "PG": "yellow",
-  "PG-13": "orange",
-  "R": "red"
-}
-
-var filterRates  =  quotes.filter(d => {
-  return d.rating === document.getElementById("rates").value
-})
-
-
-var quotesList = d3.select("#quotes")
-  .style("list-style", "none")
-  
-
-function enterData (data){
-  quotesList
-  .selectAll("li")
-  .data(data)
-  .enter()
-  .append("li")
-  .text(d => '"' + d.quote + '" - ' + d.movie + ' ('+ d.year +')')
-    .style("margin", "20px")
-    .style("padding","20px")
-    .style("font-size", d => d.quote.length < 25 ? "2em" : "1em")
-    .style("background-color", d => colors[d.rating])
-    .style("border-radius", "20px")
-}
-
-enterData(quotes)
-  
-  d3.select("#rates")
-    .on("change", () => {
-      var rate = d3.select("#rates").property("value")
-
-      var newData = quotes.filter(d => {
-        return d.rating === rate
-      })
-
-  quotesList
-  .selectAll("li")
-  .data(newData, d => d.quote)
-      .exit()
-      .remove()
-    
-      
-  if(rate === "all"){
-    enterData(quotes)
-  }else{
-    enterData(newData)
-  }
-})
-
-*/
-
+svgDates.append("g")
+        .style("transform","translate(50px,0)")
+        .selectAll("rect")
+        .data(uniquefirstAttemptDates)
+        .enter()
+          .append('rect')
+          .attr("width", widthBar)
+          .attr("height", (d,i) => yScale(uniquefirstAttemptDates[i]) )
+          .attr("y", (d,i) => height - yScale(uniquefirstAttemptDates[i]))
+          .attr("x", (d,i) => (paddingBar + widthBar) * i)
+          .attr("fill","green")
