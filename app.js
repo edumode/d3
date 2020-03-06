e3e5ce91e57178ff4084ae61c39629ae12f9e475
Vasquez <Cesar.Vasquez@xerox.com>
2020-03-06 14:57:23 -0600

    Bar chart

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 600 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
 
-var dataCSV = d3.csv("Reads.csv")
-console.log(dataCSV)
+var sn = "XKP541269"
 
 // get the data
 d3.csv("/Reads.csv", function(data) {
-    console.log(data)
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    
+    var filterSN = data.filter(data => data.SerialNumber == sn)
+    var barPadding = 10
+    var barWidth = (width / filterSN.length) - barPadding
 
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return d.Volume; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
+    var labelYearMonth = filterSN.map( d => d.Month + " " + d.Year)
 
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
+    var maxVolume = d3.max(filterSN, d => Number(d.Volume))
+    //console.log(filterSN)
+    
+    var yScale = d3.scaleLinear()
+                   .domain([0,maxVolume])
+                   .range([height,0])
 
+    var xScaleAxis = d3.scaleBand()
+                        .domain(labelYearMonth)
+                        .range([0, width])
 
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
-      .call(d3.axisLeft(y));
+                        console.log(labelYearMonth)
 
-  // append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
+    svg.append("g")
+        .style("transform","translate(45px, 0)")
+        .selectAll("rect")
+        .data(filterSN)
+            .enter()
+            .append("rect")
+                .attr("x", (d,i) => i * (barWidth + barPadding))
+                .attr("y", d => height - yScale(Number(d.Volume)))
+                .attr("width", barWidth)
+                .attr("height", d => yScale(Number(d.Volume)))
+                .attr("fill","steelblue")
+
+    svg.append("g")
+        .style("transform",`translate(${margin.left}px,0px)`)
+        .call(d3.axisLeft(yScale))
+
+    svg.append("g")
+        .style("transform",`translate(${margin.left}px,${height}px)`)
+        .call(d3.axisBottom(xScaleAxis))
 
 });
