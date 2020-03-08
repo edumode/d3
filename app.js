95160283459195778f27f67ea11798ab8bbe65c6
Cesar <cesarvasz@gmail.com>
2020-03-07 20:58:53 -0600

    Bar chart done

--- app.js
+++ app.js
@@ -1,55 +1,75 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+    width = 1200 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
 
-var sn = "XKP541269"
+
+
+
 
 // get the data
 d3.csv("/Reads.csv", function(data) {
     
-    var filterSN = data.filter(data => data.SerialNumber == sn)
     var barPadding = 10
-    var barWidth = (width / filterSN.length) - barPadding
-
-    var labelYearMonth = filterSN.map( d => d.Month + " " + d.Year)
-
-    var maxVolume = d3.max(filterSN, d => Number(d.Volume))
-    //console.log(filterSN)
     
-    var yScale = d3.scaleLinear()
-                   .domain([0,maxVolume])
-                   .range([height,0])
 
-    var xScaleAxis = d3.scaleBand()
-                        .domain(labelYearMonth)
-                        .range([0, width])
+    var optionsSN = data.map(d => d.SerialNumber)
+    var optionSNunique = [...new Set(optionsSN)]
+    var optionsTag = optionSNunique.map(d => {
+        return `<option value="${d}">${d}</option>`
+    })
+    document.getElementById("selectSN").innerHTML = optionsTag
+    
 
-                        console.log(labelYearMonth)
+    d3.select("#selectSN")
+        .on("change", (d,i) => {
+            var sn = document.getElementById("selectSN").value
+            console.log(sn)
 
-    svg.append("g")
-        .style("transform","translate(45px, 0)")
-        .selectAll("rect")
-        .data(filterSN)
-            .enter()
-            .append("rect")
-                .attr("x", (d,i) => i * (barWidth + barPadding))
-                .attr("y", d => height - yScale(Number(d.Volume)))
-                .attr("width", barWidth)
-                .attr("height", d => yScale(Number(d.Volume)))
-                .attr("fill","steelblue")
+            var filterSN = data.filter(data => data.SerialNumber == sn)
+            var barWidth = (width / filterSN.length) - barPadding
 
-    svg.append("g")
-        .style("transform",`translate(${margin.left}px,0px)`)
-        .call(d3.axisLeft(yScale))
+            var labelYearMonth = filterSN.map( d => d.Month + " " + d.Year)
 
-    svg.append("g")
-        .style("transform",`translate(${margin.left}px,${height}px)`)
-        .call(d3.axisBottom(xScaleAxis))
+            var maxVolume = d3.max(filterSN, d => Number(d.Volume))
+
+            
+            var yScale = d3.scaleLinear()
+                        .domain([0,maxVolume])
+                        .range([height,0])
+
+            var xScaleAxis = d3.scaleBand()
+                                .domain(labelYearMonth)
+                                .range([0, width])
+
+            svg.selectAll("g").remove()
+                
+                                
+            svg.append("g")
+                .style("transform","translate(45px, 0)")
+                .selectAll("rect")
+                .data(filterSN)
+                    .enter()
+                    .append("rect")
+                        .attr("x", (d,i) => i * (barWidth + barPadding))
+                        .attr("y", d => height - yScale(Number(d.Volume)))
+                        .attr("width", barWidth)
+                        .transition()
+                        .attr("height", d => yScale(Number(d.Volume)))
+                        .attr("fill","steelblue")
+
+            svg.append("g")
+                .style("transform",`translate(${margin.left}px,0px)`)
+                .call(d3.axisLeft(yScale))
+            
+            svg.append("g")
+                .style("transform",`translate(${margin.left}px,${height}px)`)
+                .call(d3.axisBottom(xScaleAxis))
+    })
 
 });
